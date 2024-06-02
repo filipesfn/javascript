09e538ec3303304e3b0060fc1da090bd7076b416
Filipe SFN <148567553+filipesfn@users.noreply.github.com>
2024-06-01 21:01:57 -0300

    Add files via upload

---
+++ Aula1 java/index.js
@@ -0,0 +1,59 @@
+// let nome = prompt("Digite seu nome")
+// // let nome = Filipe
+// let idade = Number(prompt("Digite sua idade"))
+// // let idade = 19
+// let temCarteira = true
+// let altura = Number(prompt("Digite sua altura"))
+// // let altura = 1.74
+
+// console.log(nome)
+// // console.log("Meu nome é"+nome)
+// console.log(idade)
+// // console.log("A idade é"+idade)
+// console.log(temCarteira)
+// // console.log("Tem carteira?"+temCarteira)
+// console.log(altura)
+// // console.log("Sua altura é"+altura)
+
+// //Outra forma de fazer o exercicio
+
+// console.log(`Meu nome é ${nome}`)
+// console.log(`A idade é $(idade)`)
+// console;log() 
+// console.log(`Sua altura é ${altura}`)
+
+// let soma
+// let a = 6
+// let b = 3
+// let soma = a + b
+// let subtracao = a - b
+// let multiplicacao = a*b
+// let divisao = a/3
+// let exp = a**b
+// let resto = a%b
+
+
+// console.log(`${a} + ${b} = ${soma}`)
+// console.log(`${a} - ${b} = ${subtracao}`)
+// console.log(`${a} * ${b} = ${multiplicacao}`)
+// console.log(`${a} / ${b} = ${divisao}`)
+// console.log(`${a} ^ ${b} = ${exp}`)
+// console.log(`${a} resto ${b} = ${resto}`)
+
+// let a = 10
+// let b = 2
+// let divisão = a / b
+// console.log(divisão)
+
+// let numero1 = Number(prompt("Digite um numero: "))
+// let numero2 = Number(prompt("Digite outro numero: "))
+// let multiplicação = numero1*numero2
+// console.log(multiplicação)
+
+let soma = 5
+let numero = Number(prompt("Digite um numero:"))
+soma += numero
+
+console.log(soma)
+ 
+
