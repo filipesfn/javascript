d7d630d165ec2a830445609b7a94d00d0531af54
Filipe SFN <148567553+filipesfn@users.noreply.github.com>
2024-06-22 20:55:05 -0300

    Add files via upload

---
+++ Aula4js/script.js
@@ -0,0 +1,96 @@
+// let soma = 0
+
+// while (true) {
+//     let numero = Number(prompt("Digite um número\n Digite 0 para sair"))
+// if (isNaN(numero)) {
+//     alert("Digite um número válido!")
+//     continue
+// }
+
+
+
+// if (numero==0){
+//     break
+// }
+
+// soma=numero+soma
+
+// }
+
+// alert("O valor da soma de todo os número digitados é:" +soma)
+
+
+
+
+// while (true) {
+//     let usuario = prompt("Digite seu nome: ")
+//     let senha = prompt("Digite uma nova senha: ")
+    
+// if (senha == usuario){
+//     alert("A senha não poder ser igual ao nome do usuário, por gentileza digite uma nova senha!")
+//     continue
+// }
+
+// alert("Usuário e senha válidos!! O seu usuário é: " +usuario+" e a senha é: " +senha)
+// break
+
+// }
+
+// let numero = parseInt(prompt("Digite um número: "))
+
+// if(isNaN(numero)){
+//     alert("Digite um número válido!")
+// }else{
+//     console.log("-----Tabuada de " +numero)
+// }
+
+// for (let i = 0; i<=10; i++ ){
+//     console.log(`${numero} * ${i} = ${numero* i}`);
+// }
+
+
+
+// let numero = parseInt(prompt("Digite um número: "))
+
+// if(isNaN(numero)){
+//     alert("Digite um número válido!")
+// }else{
+//     if (numero>0) {
+//         for (let i = numero; i>0; i--){
+//             console.log(i)
+
+//     }
+// }}       
+
+// let numero = parseInt(prompt("Digite um número: "))
+// if(isNaN(numero)){
+//     alert("Digite um número válido!")
+// }else{
+//     if(numero>0){
+//         for (let i = numero; i>0; i--)
+
+//     }
+// }
+
+// let texto = "Eu gosto de sushi"
+
+// for(letra of texto){
+//     console.log("O caracter da vez é ---> "+letra+" <---")
+// }
+
+//  Explicação do exercicio acima
+
+// let string = "Eu gosto de sushi"
+
+// for(caracterDaString of string){
+//     console.log("O caracter da vez é ---> "+caracterDaString+" <---")
+// }
+
+
+
+
+
+
+
+
+
