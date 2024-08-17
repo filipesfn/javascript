d0e39403b49644ad0ddc07c4e4d0f992fdbb58eb
Filipe SFN <148567553+filipesfn@users.noreply.github.com>
2024-08-17 20:56:17 -0300

    Add files via upload

---
+++ aula11js/script.js
@@ -0,0 +1,38 @@
+//let frutas = ["Manga", "Caju", "Cajá", "Graviola"]
+
+//console.log(frutas[])
+
+//for (let i = 0; i < frutas.length; i++) {
+//    console.log(frutas[i]);
+//}
+
+//const frutas = ["Manga", "Caju", "Cajá", "Graviola"]
+
+//let salada_de_frutas = frutas.join(",")
+//console.log(salada_de_frutas)
+
+//let frutas = ["Manga", "Cajú", "Cajá", "Graviola"]
+
+//let $lista = document.getElementById("lista")
+
+//console.log($lista)
+
+//frutas.forEach((fruta)=>{
+    
+//$lista.innerHTML+=`<li>${fruta}</li>`
+
+//})
+
+let ferramentas = ["MARTELO", "PREGO", "SERROTE", "RÉGUA"]
+
+let ferramentasMinusculas = ferramentas.map((f)=>{
+  return f.toLowerCase()
+})
+
+console.log(ferramentas)
+console.log(ferramentasMinusculas)
+
+console.log(numeros)
+console.log(dobro)
+
+
